95c7124cc336df7388ed37aff1ba89a95426aedb
azu <azuciao@gmail.com>
2022-11-16 22:29:02 +0900

    fix

--- bin/technological-book-corpus-ja.js
+++ bin/technological-book-corpus-ja.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { findByPattern, get } from "../";
+import { findByPattern, get } from "../index.js";
 if (process.argv[2]) {
     console.log(findByPattern(process.argv[2]).join("\n"));
 } else {
